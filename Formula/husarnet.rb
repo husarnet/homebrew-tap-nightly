bf7e1c1e6643f47ac44f3dc7ee5e7c535604eda3
ympek <sz@ympek.net>
2023-03-14 15:50:16 +0100

    add version to formula

--- Formula/husarnet.rb
+++ Formula/husarnet.rb
@@ -1,6 +1,7 @@
 class Husarnet < Formula
   desc "Low latency P2P VPN solution for robotics and more"
   homepage "https://husarnet.com"
+  version "2.0.82"
   url "https://nightly.husarnet.com/husarnet-macos-2.0.82-arm64.tar.gz"
   sha256 "9a1f412bd75f584e9b93fc91f8c824d70d0f31c29ab3ac1507516354adc83c84"
   license ""
