3af5432c1927ead59cf43b247c836551e4601555
ympek <sz@ympek.net>
2023-03-14 13:17:20 +0100

    fix some complaints from the bot

--- Formula/husarnet.rb
+++ Formula/husarnet.rb
@@ -8,19 +8,11 @@ class Husarnet < Formula
   def install
     bin.install "husarnet-macos-arm64" => "husarnet"
     bin.install "husarnet-daemon-unix-macos_arm64" => "husarnet-daemon"
-    system "#{bin}/husarnet daemon service-install"
+    # will hopefully install the service in launchd
+    system "#{bin}/husarnet", "daemon", "service-install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test husarnet-macos-2.0.64-arm64`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "husarnet version"
+    system "#{bin}/husarnet", "version"
   end
 end
