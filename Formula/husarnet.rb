2d99b8e4977a0e84395ae18027633e52c3aef112
ympek <sz@ympek.net>
2023-03-14 17:34:22 +0100

    update formula

--- Formula/husarnet.rb
+++ Formula/husarnet.rb
@@ -1,7 +1,7 @@
 class Husarnet < Formula
   desc "Low latency P2P VPN solution for robotics and more"
   homepage "https://husarnet.com"
-  version "2.0.83"
+  version "2.0.84" # fake bump, to reload homebrew and figure out its behavior
   url "https://nightly.husarnet.com/husarnet-macos-2.0.83-arm64.tar.gz"
   sha256 "b28449b7cb5adb5985018af4c162da7c2c430105e1ddd3dab2095e1f25df877b"
   license ""
@@ -11,6 +11,22 @@ class Husarnet < Formula
     bin.install "husarnet-daemon-unix-macos_arm64" => "husarnet-daemon"
     # will hopefully install the service in launchd
     # system "#{bin}/husarnet", "daemon", "service-install"
+  end
+
+  def post_install
+    system "#{bin}/husarnet-daemon version"
+    system "#{bin}/husarnet daemon service-install"
+  end
+
+  def caveats
+    s = <<~EOS
+      If service installation fails, execute:
+        sudo husarnet daemon service-install
+      To start husarnet daemon and enable it on startup.
+
+      Enjoy!
+    EOS
+    s
   end
 
   test do
