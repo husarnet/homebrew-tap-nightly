b7b1f2ecedbf03df423f35ccc91bb50222b4db08
ympek <sz@ympek.net>
2023-03-14 17:56:57 +0100

    update formula again

--- Formula/husarnet.rb
+++ Formula/husarnet.rb
@@ -1,7 +1,7 @@
 class Husarnet < Formula
   desc "Low latency P2P VPN solution for robotics and more"
   homepage "https://husarnet.com"
-  version "2.0.84" # fake bump, to reload homebrew and figure out its behavior
+  version "2.0.85" # fake bump, to reload homebrew and figure out its behavior
   url "https://nightly.husarnet.com/husarnet-macos-2.0.83-arm64.tar.gz"
   sha256 "b28449b7cb5adb5985018af4c162da7c2c430105e1ddd3dab2095e1f25df877b"
   license ""
@@ -9,13 +9,11 @@ class Husarnet < Formula
   def install
     bin.install "husarnet-macos-arm64" => "husarnet"
     bin.install "husarnet-daemon-unix-macos_arm64" => "husarnet-daemon"
-    # will hopefully install the service in launchd
-    # system "#{bin}/husarnet", "daemon", "service-install"
   end
 
   def post_install
-    system "#{bin}/husarnet-daemon version"
-    system "#{bin}/husarnet daemon service-install"
+    print "Will attempt to install the service. This requires sudo privileges."
+    system "sudo", "#{bin}/husarnet", "daemon", "service-install"
   end
 
   def caveats
@@ -23,7 +21,6 @@ class Husarnet < Formula
       If service installation fails, execute:
         sudo husarnet daemon service-install
       To start husarnet daemon and enable it on startup.
-
       Enjoy!
     EOS
     s
