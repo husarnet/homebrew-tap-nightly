e926fe579539c61565d51f4cdf69d3ff392bb35c
Szymon Niemiec <sz@ympek.net>
2023-04-27 22:59:28 +0200

    husarnet 2.0.134
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/husarnet.rb
+++ Formula/husarnet.rb
@@ -1,9 +1,9 @@
 class Husarnet < Formula
   desc "Low latency P2P VPN solution for robotics and more"
   homepage "https://husarnet.com"
-  version "2.0.85" # fake bump, to reload homebrew and figure out its behavior
-  url "https://nightly.husarnet.com/husarnet-macos-2.0.83-arm64.tar.gz"
-  sha256 "b28449b7cb5adb5985018af4c162da7c2c430105e1ddd3dab2095e1f25df877b"
+  version "2.0.134" # fake bump, to reload homebrew and figure out its behavior
+  url "https://nightly.husarnet.com/husarnet-macos-2.0.134-arm64.tar.gz"
+  sha256 "2788d3c48564b81bf8fe0aaba8f632d82bcf1b9d0b4dbd1a6c9f3cbc9f3f7c0f"
   license ""
 
   def install
