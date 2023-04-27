6303b8a7d92a68f54cd74d2af267f6fcf934ecf9
Szymon Niemiec <sz@ympek.net>
2023-04-28 00:14:42 +0200

    Husarnet for MacOS version 2.0.135
    Automatic commit by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/husarnet.rb
+++ Formula/husarnet.rb
@@ -1,9 +1,9 @@
 class Husarnet < Formula
   desc "Low latency P2P VPN solution for robotics and more"
   homepage "https://husarnet.com"
-  url "https://nightly.husarnet.com/husarnet-macos-2.0.134-arm64.tar.gz"
-  version "2.0.134"
-  sha256 "2788d3c48564b81bf8fe0aaba8f632d82bcf1b9d0b4dbd1a6c9f3cbc9f3f7c0f"
+  url "https://nightly.husarnet.com/husarnet-macos-2.0.135-arm64.tar.gz"
+  version "2.0.135"
+  sha256 "9fa7857ace8f66effb35b238da39de8e4c7496d0fd493c3835d2335df1d53c4e"
   license ""
 
   def install
